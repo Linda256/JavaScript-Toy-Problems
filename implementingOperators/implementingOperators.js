edd42c1b59cd346d7759371e1f4e33a1a0307853
Linda Wang <lindawang256@gmail.com>
2018-05-30 09:43:27 -0700

    add multiply to implementingOperators.js

--- implementingOperators/implementingOperators.js
+++ implementingOperators/implementingOperators.js
@@ -12,14 +12,40 @@
 
 var multiply = function(x, y) {
   // TODO: should return the product of x * y
+
+  let result = 0;
+  let val = Math.abs(x);
+  for (let i=0;i<Math.abs(y);i++){
+    result =result + val;
+  }
+  if ((x<0 && y>0) || (x>0 && y<0)){
+    result = - result;
+  }
+  return result;
 };
 
 var divide = function(x, y) {
   // TODO: should return the quotient of x / y
+  if (y===0) return undefined;
+  let result = 0;
+  let a = Math.abs(x);
+  let b = Math.abs(y);
+  while (a>=b){
+    a=a-b;
+    result++;
+  }
+  if ((x<0 && y>0) || (x>0 && y<0)){
+    result = - result;
+  }
+  return result;
 };
 
 var modulo = function(x, y) {
   // TODO: should return the remainder of x / y
 };
 
+let m = multiply(-5,2);
+console.log(m);
 
+let d = divide(-15,0);
+console.log(d);
