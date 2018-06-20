6c8f6aa9a24144e191a5879aabdacc9f293e4d1e
Linda Wang <lindawang256@gmail.com>
2018-06-20 10:13:20 -0700

    non occurance char

--- nonrepeatedCharacter/nonrepeatedCharacter.js
+++ nonrepeatedCharacter/nonrepeatedCharacter.js
@@ -5,7 +5,33 @@
  *   firstNonRepeatedCharacter('ABA'); // => 'B'
  *   firstNonRepeatedCharacter('AACBDB'); // => 'C'
  */
-
-var firstNonRepeatedCharacter = function(string) {
+/*
+O: Char
+I:String
+C: No
+E: lowercase vs. upcase
+*/
+var firstNonRepeatedCharacter = function(str) {
   // TODO: your solution here
+
+  //create an object (visited) to store the char visited
+  // loop through the string,
+  //  check the char(convert to lower case) is in visited or not
+        // if yes, value++
+        // if no, put str[i] into visisted, set the value =1 ;
+  // loop through visited. return the first key with value 1. ;
+  let visited ={};
+  for (let i=0;i<str.length;i++){
+    let currChar = str[i];
+    visited[currChar] === undefined ? visited[currChar] = 1 : visited[currChar] = visited[currChar]+1;
+    }
+  for (k in visited){
+    if (visited[k]===1) return k;
+  }
+  return null;
 };
+
+//let result=firstNonRepeatedCharacter('ABA');
+//let result=firstNonRepeatedCharacter('AACBCDB');
+let result=firstNonRepeatedCharacter('AAAACXC');
+console.log(result);
