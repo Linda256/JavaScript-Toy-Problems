4a88ef89e83af131c9349d6aa81087a5a385adb0
Linda Wang <lindawang256@gmail.com>
2018-06-22 18:45:29 -0700

    nthFibonacci.js

--- nthFibonacci/nthFibonacci.js
+++ nthFibonacci/nthFibonacci.js
@@ -19,10 +19,56 @@
  * etc...
  *
  */
+ /*
+ O:Number
+ I:Number
+ C:No
+ E:0 return? 1 return 1
+ n=5
+ 0 1 1 2 3 5 return 5
+ n=6
+  0 1 1 2 3 5 8 return 8
+
+
+ if n===0 return 0
+ arr = [0,1]
+ pre = arr[0];
+ curr = arr[1];
+ i = 1
+ while (i<n) n=3
+
+     curr=curr+pre curr =0+1 =1 curr=
+     push curr into arr  [0,1,1]
+     i++    2
+     pre = arr[i-1]
+     curr = arr[i]
+return curr
+
+
+
+
+ */
+
 
 var nthFibonacci = function (n) {
   // TODO: implement me!
+  if (n===0) return 0;
+  let arr = [0,1]
+  let pre = arr[0];
+  let curr = arr[1];
+  i = 1
+  while (i<n-1) {
+    curr=curr+pre
+    arr.push(curr);
+    i++
+    pre = arr[i-1]
+    curr = arr[i]
+  }
+  return curr+pre;
 };
+
+let result = nthFibonacci(6);
+console.log(result);
 
 
 
