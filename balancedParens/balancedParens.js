c3080dc62b7fe48df7e35426b7e88855072cc1d3
Linda Wang <lindawang256@gmail.com>
2018-07-11 11:26:34 -0700

    Done balancedParens.js

--- balancedParens/balancedParens.js
+++ balancedParens/balancedParens.js
@@ -22,8 +22,57 @@
  * balancedParens(' var hubble = function() { telescopes.awesome();'); // false
  *
  *
+ O: boolean
+ I: char '(' '{' '['
+ C: O(n)
+ E: if input is not '(' '{' '['?
+
+ left = ['(','[','{']
+ right=[')',']','}']
+
+loop through the input
+if char in left i, push the right[i] into stack;
+else
+    if char in right i, compare right[i] with stack.pop();
+        if equel, continue
+        not  return false
+return false;
  */
 var balancedParens = function(input) {
+  let left = ['(','[','{'];
+  let right=[')',']','}'];
+  let stack=[];
+  for (let k=0; k<input.length; k++){
+    if (left.indexOf(input[k]) === -1 && right.indexOf(input[k]) === -1) continue;
+    if (right.indexOf(input[k]) !== -1 && stack.length===0) return false;
+    if (left.indexOf(input[k]) !== -1){
+      let i = left.indexOf(input[k]);
+      stack.push(right[i]);
+      continue;
+    }
+    if (right.indexOf(input[k]) !== -1){
+      if (stack.pop() === input[k]){
+        console.log(input[k]);
+        continue;
+      }else {
+        return false;
+      }
+    }
+  }
+  console.log(stack.length);
+ if (stack.length===0) {
+  return true;
+ } else {
+    return false;
+  }
 };
+
+
+//let result = balancedParens('(())[](()){}');
+//let result = balancedParens(' var hubble = function() { telescopes.awesome();');
+//let result=balancedParens(' var wow  = { yo: thisIsAwesome() }');
+let result=balancedParens(')(')
+console.log(result);
+
 
 
